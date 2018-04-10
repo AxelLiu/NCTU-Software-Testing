207c388179240f42427d4d6c32a15828b232c363
axel831102 <axel831102@gmail.com>
2018-04-10 23:15:20 +0800

    Add Boundary Normal's Test

---
+++ hw1_unittest.cc
@@ -0,0 +1,19 @@
+#include "hw1.h"
+#include "gtest/gtest.h"
+
+TEST(BoundaryValue, Normal)
+{
+		EXPECT_EQ("ISOSCELES", IsTriangle(1, 100, 100));
+		EXPECT_EQ("ISOSCELES", IsTriangle(2, 100, 100));
+		EXPECT_EQ("ISOSCELES", IsTriangle(199, 100, 100));
+		EXPECT_EQ("ISOSCELES", IsTriangle(200, 100, 100));
+		EXPECT_EQ("ISOSCELES", IsTriangle(100, 1, 100));
+		EXPECT_EQ("ISOSCELES", IsTriangle(100, 2, 100));
+		EXPECT_EQ("ISOSCELES", IsTriangle(100, 199, 100));
+		EXPECT_EQ("ISOSCELES", IsTriangle(100, 200, 100));
+		EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 1));
+		EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 2));
+		EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 199));
+		EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 200));
+		EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 100));
+}
