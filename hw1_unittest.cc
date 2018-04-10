80d78c1663f1724228259a5fce8e66ec42b54af8
axel831102 <axel831102@gmail.com>
2018-04-11 01:49:46 +0800

    Add Boundary Robust's Test

--- hw1_unittest.cc
+++ hw1_unittest.cc
@@ -3,17 +3,27 @@
 
 TEST(BoundaryValue, Normal)
 {
-		EXPECT_EQ("ISOSCELES", IsTriangle(1, 100, 100));
-		EXPECT_EQ("ISOSCELES", IsTriangle(2, 100, 100));
-		EXPECT_EQ("ISOSCELES", IsTriangle(199, 100, 100));
-		EXPECT_EQ("INVALID", IsTriangle(200, 100, 100));
-		EXPECT_EQ("ISOSCELES", IsTriangle(100, 1, 100));
-		EXPECT_EQ("ISOSCELES", IsTriangle(100, 2, 100));
-		EXPECT_EQ("ISOSCELES", IsTriangle(100, 199, 100));
-		EXPECT_EQ("INVALID", IsTriangle(100, 200, 100));
-		EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 1));
-		EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 2));
-		EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 199));
-		EXPECT_EQ("INVALID", IsTriangle(100, 100, 200));
-		EXPECT_EQ("EQUILATERAL", IsTriangle(100, 100, 100));
+	EXPECT_EQ("ISOSCELES", IsTriangle(1, 100, 100));
+	EXPECT_EQ("ISOSCELES", IsTriangle(2, 100, 100));
+	EXPECT_EQ("ISOSCELES", IsTriangle(199, 100, 100));
+	EXPECT_EQ("INVALID", IsTriangle(200, 100, 100));
+	EXPECT_EQ("ISOSCELES", IsTriangle(100, 1, 100));
+	EXPECT_EQ("ISOSCELES", IsTriangle(100, 2, 100));
+	EXPECT_EQ("ISOSCELES", IsTriangle(100, 199, 100));
+	EXPECT_EQ("INVALID", IsTriangle(100, 200, 100));
+	EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 1));
+	EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 2));
+	EXPECT_EQ("ISOSCELES", IsTriangle(100, 100, 199));
+	EXPECT_EQ("INVALID", IsTriangle(100, 100, 200));
+	EXPECT_EQ("EQUILATERAL", IsTriangle(100, 100, 100));
+}
+
+TEST(BoundaryValue, Robust)
+{
+	EXPECT_EQ("INVALID", IsTriangle(0, 100, 100));
+	EXPECT_EQ("INVALID", IsTriangle(201, 100, 100));
+	EXPECT_EQ("INVALID", IsTriangle(100, 0, 100));
+	EXPECT_EQ("INVALID", IsTriangle(100, 201, 100));
+	EXPECT_EQ("INVALID", IsTriangle(100, 100, 0));
+	EXPECT_EQ("INVALID", IsTriangle(100, 100, 201));
 }
