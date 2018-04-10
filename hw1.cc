eac2f102c87b3b6e77558b840e6f4189206c20ab
axel831102 <axel831102@gmail.com>
2018-04-11 01:36:11 +0800

    Add feature code

--- hw1.cc
+++ hw1.cc
@@ -4,5 +4,14 @@
 
 std::string IsTriangle(int a, int b, int c)
 {
-	return "";
+	if(a < 1 || a > 200 || b < 1 || b > 200 || c < 1 || c > 200)
+		return "INVALID";
+	if(a + b <= c || b + c <= a || a + c <= b)
+		return "INVALID";
+	if(a==b && a==c)
+		return "EQUILATERAL";
+	if((a == b && b != c) || (a == c && a != b) || (b == c && a != b))
+		return "ISOSCELES";
+
+	return "UNEQUAL";
 }
