a58a3d9909c8c7662842df8f10ea21dc8919a8f7
axel831102 <axel831102@gmail.com>
2018-05-14 22:32:23 +0800

    Add C0 testing for triangle

--- hw2/triangle/triangle_unittest.cc
+++ hw2/triangle/triangle_unittest.cc
@@ -3,5 +3,9 @@
 
 TEST(PathTesting, C0)
 {
-	EXPECT_EQ("ISOSCELES", IsTriangle(1, 100, 100));
+	EXPECT_EQ("SCALENE", IsTriangle(3, 4, 5));
+	EXPECT_EQ("INVALID", IsTriangle(0, 4, 5));
+	EXPECT_EQ("INVALID", IsTriangle(1, 1, 2));
+	EXPECT_EQ("EQUILATERAL", IsTriangle(5, 5, 5));
+	EXPECT_EQ("ISOSCELES", IsTriangle(2,2,1));
 }
