2414bf163ba8889f03e627766b3f4ffdf22feaf3
axel831102 <axel831102@gmail.com>
2018-05-14 22:36:17 +0800

    Add C1 C2 for triangle

--- hw2/triangle/triangle_unittest.cc
+++ hw2/triangle/triangle_unittest.cc
@@ -9,3 +9,21 @@ TEST(PathTesting, C0)
 	EXPECT_EQ("EQUILATERAL", IsTriangle(5, 5, 5));
 	EXPECT_EQ("ISOSCELES", IsTriangle(2,2,1));
 }
+
+TEST(PathTesting, C1)
+{
+	EXPECT_EQ("SCALENE", IsTriangle(6, 8, 10));
+	EXPECT_EQ("INVALID", IsTriangle(0, 5, 15));
+	EXPECT_EQ("INVALID", IsTriangle(4, 4, 10));
+	EXPECT_EQ("EQUILATERAL", IsTriangle(10, 10, 10));
+	EXPECT_EQ("ISOSCELES", IsTriangle(10,10,1));
+}
+
+TEST(PathTesting, C2)
+{
+	EXPECT_EQ("SCALENE", IsTriangle(6, 8, 10));
+	EXPECT_EQ("INVALID", IsTriangle(0, 5, 15));
+	EXPECT_EQ("INVALID", IsTriangle(4, 4, 10));
+	EXPECT_EQ("EQUILATERAL", IsTriangle(10, 10, 10));
+	EXPECT_EQ("ISOSCELES", IsTriangle(10,10,1));
+}
