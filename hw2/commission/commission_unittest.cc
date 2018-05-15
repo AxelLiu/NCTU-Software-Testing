7ec9d8406b3319f48b1f3597c0b1a9e3abf07ffe
axel831102 <axel831102@gmail.com>
2018-05-15 22:08:26 +0800

    Add MCDC for commission

--- hw2/commission/commission_unittest.cc
+++ hw2/commission/commission_unittest.cc
@@ -24,3 +24,17 @@ TEST(PathTesting, C2)
     EXPECT_EQ(115.0, commission(11,11,11));
     EXPECT_EQ(260.0, commission(20,20,20));
 }
+
+TEST(PathTesting, MCDC)
+{
+    // invalid == -1.0
+    EXPECT_EQ(-1.0, commission(0,20,20));
+    EXPECT_EQ(-1.0, commission(0,20,20));
+    EXPECT_EQ(-1.0, commission(71,20,20));
+    EXPECT_EQ(-1.0, commission(20,0,20));
+    EXPECT_EQ(-1.0, commission(20,20,91));
+    EXPECT_EQ(10.0, commission(1,1,1));
+    EXPECT_EQ(121.75, commission(1,20,20));
+    EXPECT_EQ(260.0, commission(20,20,20));
+    EXPECT_EQ(-1.0, commission(-1,20,20));
+}
