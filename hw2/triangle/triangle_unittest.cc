1974a0126ac1bbd0910afb0cad25afd0779663d6
axel831102 <axel831102@gmail.com>
2018-05-15 21:40:49 +0800

    Add MCDC for triangle

--- hw2/triangle/triangle_unittest.cc
+++ hw2/triangle/triangle_unittest.cc
@@ -27,3 +27,22 @@ TEST(PathTesting, C2)
 	EXPECT_EQ("EQUILATERAL", IsTriangle(10, 10, 10));
 	EXPECT_EQ("ISOSCELES", IsTriangle(10,10,1));
 }
+
+TEST(PathTesting, MCDC)
+{	
+	EXPECT_EQ("INVALID", IsTriangle(5, 5, 201));
+	EXPECT_EQ("EQUILATERAL", IsTriangle(5, 5, 5));
+	EXPECT_EQ("INVALID", IsTriangle(0, 5, 5));
+	EXPECT_EQ("INVALID", IsTriangle(201, 5, 5));
+	EXPECT_EQ("INVALID", IsTriangle(5, 201, 5));
+	EXPECT_EQ("INVALID", IsTriangle(5, 0, 5));
+	EXPECT_EQ("INVALID", IsTriangle(5, 5, 0));
+	EXPECT_EQ("INVALID", IsTriangle(-1, -1, -1));
+	EXPECT_EQ("SCALENE", IsTriangle(2,3,4));
+	EXPECT_EQ("ISOSCELES", IsTriangle(4,5,5));
+	EXPECT_EQ("ISOSCELES", IsTriangle(5,5,4));
+	EXPECT_EQ("EQUILATERAL", IsTriangle(6,6,6));
+	EXPECT_EQ("ISOSCELES", IsTriangle(3,3,5));
+	EXPECT_EQ("ISOSCELES", IsTriangle(3,5,3));
+	EXPECT_EQ("ISOSCELES", IsTriangle(5,3,3));
+}
