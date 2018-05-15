61edaa84aaf4cb796667d8e4a3d9a4466fccccfc
axel831102 <axel831102@gmail.com>
2018-05-15 21:26:03 +0800

    Add C1 C2 for commission

--- hw2/commission/commission_unittest.cc
+++ hw2/commission/commission_unittest.cc
@@ -1,10 +1,26 @@
 #include "gtest/gtest.h"
 #include "commission.h"
 
-TEST(Pathtesting, C0)
+TEST(PathTesting, C0)
 {
     EXPECT_EQ(10.0, commission(1,1,1));
     EXPECT_EQ(-1.0, commission(0,30,25));
     EXPECT_EQ(103.75, commission(10,10,11));
+    EXPECT_EQ(260.0, commission(20,20,20));
+}
+
+TEST(PathTesting, C1)
+{
+    EXPECT_EQ(10.0, commission(1,1,1));
+    EXPECT_EQ(-1.0, commission(0,30,25));
+    EXPECT_EQ(115.0, commission(11,11,11));
+    EXPECT_EQ(260.0, commission(20,20,20));
+}
+
+TEST(PathTesting, C2)
+{
+    EXPECT_EQ(10.0, commission(1,1,1));
+    EXPECT_EQ(-1.0, commission(0,30,25));
+    EXPECT_EQ(115.0, commission(11,11,11));
     EXPECT_EQ(260.0, commission(20,20,20));
 }
